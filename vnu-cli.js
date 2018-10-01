a34bb50bb8a84980edaafcba6e68056bb9589bf8
Gonzalo <gcordero@salesforce.com>
2018-09-30 22:02:08 -0700

    adding the vnu-cli

---
+++ vnu-cli.js
@@ -0,0 +1,44 @@
+const exec = require ( 'child_process' ).exec;
+const vnu = require ( 'vnu-jar' );
+const fs = require('fs');
+
+function constructHTML (template) {
+    const html = `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>H</title><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>${template}</body></html>`;
+
+    return html;
+}
+
+function validateHTML(HTML) {
+    const html = constructHTML(HTML);
+    // removing previous validate file
+    fs.existsSync('validate.html', (exists) => {
+        if (exists) {
+            fs.unlinkSync('validate.html', (error) => {
+                throw error;
+            });
+        }
+    });
+
+    fs.writeFileSync("validate.html", html, (err) => {
+        if(err) {
+            throw (err);
+        }
+    });
+
+    return  executeVNU(html);
+}
+
+function executeVNU(html) {
+    return new Promise((resolve, reject) => {
+        exec (`java -jar ${vnu} validate.html`, ( error, results ) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(results)
+        });
+    });
+}
+
+module.exports = {
+    validateHTML
+}
