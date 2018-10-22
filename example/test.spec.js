b3947be3762fea79c15c1f23a1709b16627eaa3c
Gonzalo <gcordero@salesforce.com>
2018-10-22 00:06:49 -0700

    adding expect.extend

--- example/test.spec.js
+++ example/test.spec.js
@@ -1,21 +1,26 @@
-const {  validateHTML } = require('../vnu-cli')
+const { validateHTML, toContainCorrectMarkUp } = require('../vnu-cli');
+
 
 describe('vnu-cli', () => {
     it('should validate correct valid markup', async () => {
         const html = '<p><span></span></p>';
+        expect.extend(toContainCorrectMarkUp);
+        
         try {
-            expect(await validateHTML(html));
+            expect(await validateHTML(html)).toContainCorrectMarkUp();
         } catch (error) {
             throw error;
         }
-    })
+    });
 
     it('should fail with incorrect markup', async () => {
         const html = '<span><p></p></span>';
+        expect.extend(toContainCorrectMarkUp);
+
         try {
-            expect(await validateHTML(html));
+            expect(await validateHTML(html)).toContainCorrectMarkUp();
         } catch (error) {
             throw error;
         }
-    })
+    });
 });
--- vnu-cli.js
+++ vnu-cli.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 
 function constructHTML (template) {
     const html = `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>H</title><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>${template}</body></html>`;
-
+    
     return html;
 }
 
 function validateHTML(HTML) {
     const html = constructHTML(HTML);
+
     // removing previous validate file
     fs.existsSync('validate.html', (exists) => {
         if (exists) {
@@ -25,20 +26,39 @@ function validateHTML(HTML) {
         }
     });
 
-    return  executeVNU(html);
-}
 
-function executeVNU(html) {
     return new Promise((resolve, reject) => {
         exec (`java -jar ${vnu} validate.html`, ( error, results ) => {
             if (error) {
-                reject(error);
+                resolve({error : error.message})
+            } else {
+                resolve({result : "no violations"})
             }
-            resolve(results)
         });
     });
 }
 
+
+const toContainCorrectMarkUp = {
+    toContainCorrectMarkUp(results) {
+        if (results.error) {
+            const message = results.error.substr(results.error.lastIndexOf('error: '));
+            return {
+                message: () =>
+                message,
+                pass: false,
+            };        
+        } else {
+            return {
+                message: () =>
+                results.result,
+                pass: true,
+            };        
+        }
+    },
+}
+
 module.exports = {
+    toContainCorrectMarkUp,
     validateHTML
 }
